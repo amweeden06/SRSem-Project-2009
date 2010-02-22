63230f571166add1f741cd1549ea7a3367bd7394
amweeden06 <amweeden.earlham@gmail.com>
2010-02-22 10:32:29 -0500

    Adding save/load functionality and initial data collection

--- Source/ACL/GameEngine.cpp
+++ Source/ACL/GameEngine.cpp
@@ -8,6 +8,7 @@ namespace Sewers
 {
 	GameEngine::GameEngine(string rooms_file)
 	{	
+		set_num_steps(0);
 		set_rooms_file(rooms_file);
 		
 		_num_switches = _num_gates = 0;
@@ -37,6 +38,8 @@ namespace Sewers
         set_down_key('s');
         set_up_key('w');
 		set_action_key('f');
+		set_save_key('i');
+		set_load_key('u');
         set_quit_key('q');
 	}
 	
@@ -58,7 +61,7 @@ namespace Sewers
     // POSTCONDITION: The circuit of the room has been loaded from `filename'
 	void GameEngine::load_circuit(string filename)
 	{
-		// Used for to make extraction easy, via the >> operator
+		// Used for to make insertion easy, via the >> operator
 		ifstream ifs;
 		// The type of the current object being loaded from the file
 		string current_type = "";
@@ -86,31 +89,115 @@ namespace Sewers
 				ifs >> output_value;
 				// Add to this room's list of switches
 				_switches[_num_switches++] = (CircuitObject(current_type));
-				_switches[_num_switches-1].set_width(SWITCH_WIDTH);
-				_switches[_num_switches-1].set_height(SWITCH_HEIGHT);
-				_switches[_num_switches-1].set_output_value(output_value);
+				_switches[num_switches()-1].set_width(SWITCH_WIDTH);
+				_switches[num_switches()-1].set_height(SWITCH_HEIGHT);
+				_switches[num_switches()-1].set_output_value(output_value);
 				// Connect the object to the rest of the circuit
-				connect_object(_switches[_num_switches-1]);
+				connect_object(_switches[num_switches()-1]);
 			}
 			else if(current_type == "BUTTON")
 			{
 				// The last switch added gets the button
-				_switches[_num_switches-1].set_has_button(true);
+				_switches[num_switches()-1].set_has_button(true);
 			}			
 			else 
 			{
 				// Add to this room's list of gates
 				_gates[_num_gates++] = (CircuitObject(current_type));
 				// Update the gate in the room's list
-				_gates[_num_gates-1].set_width(GATE_WIDTH);
-				_gates[_num_gates-1].set_height(GATE_HEIGHT);
+				_gates[num_gates()-1].set_width(GATE_WIDTH);
+				_gates[num_gates()-1].set_height(GATE_HEIGHT);
 				// Connect the object to the rest of the circuit
-				connect_object(_gates[_num_gates-1]);
+				connect_object(_gates[num_gates()-1]);
 			}			
 		}
 
 		// Set up circuit for drawing
 		prepare_circuit_drawing(*(_exit.input1()), 0, 0, w, h);
+	}
+	
+	// PRECONDITIONS: None
+	// POSTCONDITION: Saves the game state to a user-specified file
+	// 
+	// Note:  Only the information that cannot be derived from other information
+	//         in the room should be saved.
+	// Note:  This function's syntax should match that of the load_game function below
+	void GameEngine::save_game() const
+	{
+		// Used to make extraction easy, via the << operator
+		ofstream ofs;
+		// User-specified file
+		string filename;
+		
+		// Prompt the user for filename
+		cerr << "SAVE GAME:  Enter filename: ";
+		cin >> filename;
+		
+		if(filename == "cancel")
+			cerr << "SAVE CANCELLED" << endl;
+		
+		// Open the file for saving
+		ofs.open(filename.c_str());
+		if(!ofs)
+        {
+			cerr << "ERROR:  Could not open file for saving" << endl;
+        }
+		else 
+		{
+			ofs << _avatar.left() << " " << _avatar.bottom() << " ";
+			ofs << _avatar.direction() << " ";
+			ofs << (_avatar.animation_frame() ? "true" : "false") << " ";
+			for(size_t i = 0; i < num_switches(); ++i)
+			{
+				if(_switches[i].has_button())
+				{
+					ofs << _switches[i].output_value() << " ";
+				}
+			}
+		}
+	}
+	
+	// PRECONDITIONS: none
+	// POSTCONDITION: The game state from a user-specified file has been loaded.
+	void GameEngine::load_game()
+	{
+		ifstream ifs;
+		string filename;
+		
+		// Prompt the user for filename
+		cerr << "LOAD GAME:  Enter filename: ";
+		cin >> filename;
+				
+		// Open the file for saving
+		ifs.open(filename.c_str());
+		if(!ifs)
+        {
+			cerr << "ERROR:  Could not open file " << filename << endl;
+        }
+		else 
+		{
+			GLint left, bottom;
+			string direction, animation_frame;
+			int value;
+			
+			ifs >> left >> bottom >> direction >> animation_frame;
+			_avatar.set_left(left);
+			_avatar.set_bottom(bottom);
+			_avatar.set_direction(direction);
+			if(animation_frame == "true")
+				_avatar.set_animation_frame(true);
+			else
+				_avatar.set_animation_frame(false);
+			
+			for(size_t i = 0; i < num_switches(); ++i)
+			{
+				if(_switches[i].has_button())
+				{
+					ifs >> value;
+					_switches[i].set_output_value(value);
+				}
+			}
+		}
 	}
 
     // PRECONDITIONS: none
@@ -195,14 +282,6 @@ namespace Sewers
 
     // Keyboard event handler
     // Wrapper for the glutKeyboardFunc
-    // Relevant keys:   left movement
-    //                  right movement
-    //                  downward movement
-    //                  upward movement
-    //                  display stats
-    //                  display help
-    //                  display options
-    //                  quit
     void GameEngine::key_click(unsigned char key, int x, int y)
     {
         if(key == left_key())
@@ -241,12 +320,23 @@ namespace Sewers
 				   }
 			}
 			
-			// See if the avatar is touching the computer
+			// Display the truth table if the player is touching the computer
+			//  and it is on
 			if(intersecting(_avatar, _computer) && _computer.is_on())
 				print_truth_table();
 			
 			draw();
 		}		
+		else if(key == save_key())
+		{
+			save_game();
+			draw();
+		}
+		else if(key == load_key())
+		{
+			load_game();
+			draw();
+		}
         else if(key == quit_key())
         {
             exit(EXIT_SUCCESS);
@@ -254,27 +344,42 @@ namespace Sewers
     }
 	
 	// PRECONDITIONS: none
-	// POSTCONITIONS: Truth table for the current room has been drawn
+	// POSTCONITIONS: Truth table for the current room has been drawn, with
+	//                 prompts for the user at each row
 	void GameEngine::print_truth_table() const
 	{
+		// Each switch is assigned a unique letter.
+		// P is often the standard choice for the initial letter.
 		char letter = 'P';
+		// For each row in the table, we will be prompting the user to
+		//  input the correct answer for that row
+		int row_answer = 0;
+		
+		// Print the column headings.  We want a column
+		//  for each switch in the circuit.
 		for(size_t i = 0; i < num_switches(); ++i)
 		{
-			cerr << letter << "    ";
+			cerr << letter << "\t";
+			// Set the letter of the next switch
 			letter++;
 		}
 		
+		// Print the final column heading.  This is the boolean
+		//  description of the current circuit.
+		// Again, the initial letter assignment is "P"
 		letter = 'P';
 		print_truth_table_result(*(_exit.input1()), letter);
 		cerr << endl;
 		
-		int row_answer = 0;
-		
+		// There will be 2^n rows, where n is the number of switches
+		//  in the circuit.
 		for(size_t i = 0; i < pow(2.0,(double)num_switches()); ++i)
 		{
+			// This section is to ensure that we have all combinations of
+			//  switches
 			for(int j = num_switches() - 1; j >= 0; --j)
 			{
-				cerr << (int)(i / pow(2.0,(double)j)) % 2 << "    ";
+				cerr << (int)(i / pow(2.0,(double)j)) % 2 << "\t";
 			}
 			cin >> row_answer;
 			cerr << endl;
@@ -323,8 +428,11 @@ namespace Sewers
 			proposed_position.right() < _exit.left() &&
 			proposed_position.bottom() > WALL_HEIGHT &&
 			proposed_position.top() < WINDOW_HEIGHT))
-			// If so, allow the avatar to move
+			// If so, allow the avatar to move and record the step
+		{
 			_avatar = proposed_position;
+			set_num_steps(num_steps()+1);
+		}
 	}
 	
 	// Find an object to which we can connect the `input' object
--- Source/ACL/GameEngine.hpp
+++ Source/ACL/GameEngine.hpp
@@ -7,6 +7,7 @@
 #include "Entrance.hpp"
 #include "Exit.hpp"
 #include "GameScreen.hpp"
+#include "CircuitObject.hpp"
 
 namespace Sewers
 {
@@ -21,6 +22,7 @@ namespace Sewers
 			// CONSTRUCTORS
 			GameEngine(string rooms_file = "Room1.sew");
 			// ACCESSORS
+			size_t num_steps() const { return _num_steps; }
 			string rooms_file() const { return _rooms_file; }
 			size_t num_switches() const { return _num_switches; }
 			size_t num_gates() const { return _num_gates; }
@@ -29,23 +31,32 @@ namespace Sewers
             unsigned char down_key() const { return _down_key; }
             unsigned char up_key() const { return _up_key; }
 			unsigned char action_key() const { return _action_key; }
+			unsigned char save_key() const { return _save_key; }
+			unsigned char load_key() const { return _load_key; }
             unsigned char quit_key() const { return _quit_key; }
             // MUTATORS
+			void set_num_steps(const size_t num_steps) { _num_steps = num_steps; }
 			void set_rooms_file(const string rooms_file) { _rooms_file = rooms_file; }
             void set_left_key(const unsigned char key) { _left_key = key; }
             void set_right_key(const unsigned char key) { _right_key = key; }
             void set_down_key(const unsigned char key) { _down_key = key; }
             void set_up_key(const unsigned char key) { _up_key = key; }
 			void set_action_key(const unsigned char key) { _action_key = key; }
+			void set_save_key(const unsigned char key) { _save_key = key; }
+			void set_load_key(const unsigned char key) { _load_key = key; }
             void set_quit_key(const unsigned char key) { _quit_key = key; }
 			// METHODS
 			void init_glut(int& argc, char** argv);
 			void load_circuit(const string filename);
+			void save_game() const;
+			void load_game();
 			// GLUT WRAPPERS
 			void draw() const;
             void key_click(const unsigned char key, const int x, const int y);
 			void print_truth_table() const;
 		private:
+			// Data collection
+			size_t _num_steps; // Number of steps user has taken in this room
 			// Room information
 			int _current_room; // ID of the current room
 			string _rooms_file; // Filename of the rooms file
@@ -61,7 +72,8 @@ namespace Sewers
 			GameScreen _button_screen;
 			GameScreen _computer_screen;
             // Keyboard event list
-            unsigned char _left_key, _right_key, _down_key, _up_key, _action_key, _quit_key;
+            unsigned char _left_key, _right_key, _down_key, _up_key;
+			unsigned char _action_key, _save_key, _load_key, _quit_key;
             // PRIVATE MEMBER FUNCTIONS
             void attempt_avatar_move(const string direction);
 			void connect_object(CircuitObject& input);
